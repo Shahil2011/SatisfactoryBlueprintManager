7fa2bfd36c1d09df1a7e1436d10b5286cc85d905
Shahil2011 <shahilpoonsamy@gmail.com>
2020-08-05 20:11:52 +0200

    Added recipes file. Implemented main UI loop and file scanning

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,82 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("I've forgotten how to write code");
+        System.out.println("Hey! Welcome to an unofficial satisfactory blueprint manager program!");
+        Scanner sc = new Scanner(System.in);
+        //System Loop
+
+        System.out.println("Please enter the number of the operation you'd like to perform:" +
+                "\n0: Exit" +
+                "\n1: View Recipe" +
+                "\n2: Add Recipe" +
+                "\n3: Build Blueprint");
+        int choice = sc.nextInt();
+        while(choice != 0)
+        {
+
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("Calling function viewRecipe()...");
+                    viewRecipe();
+                    break;
+                case 2:
+                    System.out.println("Choice 2 Made!");
+                    break;
+                case 3:
+                    System.out.println("Choice 3 Made!");
+                    break;
+                default:
+                    System.out.println("Not a valid choice. Please enter the number of the above listed options");
+            }
+
+            System.out.println("Please enter the number of the operation you'd like to perform:" +
+                    "\n0: Exit" +
+                    "\n1: View Recipe" +
+                    "\n2: Add Recipe" +
+                    "\n3: Build Blueprint");
+            choice = sc.nextInt();
+        }
+
+        System.out.println("Exiting...");
+        System.exit(0);
+    }
+
+    public static void viewRecipe()
+    {
+        System.out.println("Please enter the name of an item to look up the recipe for:");
+
+        Scanner sc = new Scanner(System.in);
+        String itemName = sc.next();
+        String item = "";
+
+        System.out.println("Checking file recipes.txt");
+        try
+        {
+            Scanner f = new Scanner(new File("recipes.txt"));
+            f.useDelimiter(",");
+            boolean itemFound = false;
+            while(f.hasNextLine() && !itemFound)
+            {
+                item = f.next();
+                if(itemName.equals(item))
+                {
+                    System.out.println("Found Item : " + item);
+                    itemFound = true;
+                    break;
+                }
+                f.nextLine();
+            }
+            if(itemFound == false)
+                System.out.println("Sorry, could not find that item");
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e);
+        }
     }
 }
