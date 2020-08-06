adb0e15886af3afb8a2b545d866c0447f30029b5
Shahil2011 <shahilpoonsamy@gmail.com>
2020-08-06 22:52:35 +0200

    Implemented function to add recipe to runtime database (not write to file). Also reworked file scanning loop, supports multiple input lengths without hard-coding a search of which building supports which input lengths

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -17,18 +17,20 @@ public class Main {
         try
         {
             Scanner f = new Scanner(new File("recipes.txt"));
-            f.useDelimiter(",|\\r|\\n");
-            while(f.hasNext())
+            //f.useDelimiter(",");
+            while(f.hasNextLine())
             {
-                String outputItem = f.next();
-                float outputRate = f.nextFloat();
-                String building = f.next();
-                String input1 = f.next();
-                float inputRate1 = f.nextFloat();
-                if(building.equals("Foundry") || building.equals("Assembler"))
+                Scanner l = new Scanner(f.nextLine());
+                l.useDelimiter(",");
+                String outputItem = l.next();
+                float outputRate = l.nextFloat();
+                String building = l.next();
+                String input1 = l.next();
+                float inputRate1 = l.nextFloat();
+                if(l.hasNext())
                 {
-                    String input2 = f.next();
-                    float inputRate2 = f.nextFloat();
+                    String input2 = l.next();
+                    float inputRate2 = l.nextFloat();
                     recipes.add(new Recipe(outputItem, outputRate, building, input1, inputRate1, input2, inputRate2));
                 }
                 else
@@ -46,7 +48,7 @@ public class Main {
         }
 
         //System Loop
-        System.out.println("Please enter the number of the operation you'd like to perform:" +
+        System.out.println("\nPlease enter the number of the operation you'd like to perform:" +
                 "\n0: Exit" +
                 "\n1: View Recipe" +
                 "\n2: Add Recipe" +
@@ -55,14 +57,31 @@ public class Main {
         while(choice != 0)
         {
 
+            String itemName;
             switch(choice)
             {
                 case 1:
-                    System.out.println("Calling function viewRecipe()...");
-                    viewRecipe();
+                    System.out.println("Please enter the name of an item to look up the recipe for (Use no spaces, capitalise the beginning of each word, and use singular form)" +
+                            "\nEnter 'all' to view all recipes");
+
+                    itemName = sc.next();
+                    viewRecipe(itemName);
                     break;
                 case 2:
-                    System.out.println("Choice 2 Made!");
+                    System.out.println("Please enter the name of an item to create a recipe for: ");
+                    itemName = sc.next();
+                    if(findRecipe(itemName))
+                    {
+                        System.out.println("Sorry, that recipe already exists");
+                        break;
+                    }
+                    else if(itemName.equals("all"))
+                    {
+                        System.out.println("Invalid option");
+                        break;
+                    }
+                    else
+                        addRecipe(itemName);
                     break;
                 case 3:
                     System.out.println("Choice 3 Made!");
@@ -71,7 +90,7 @@ public class Main {
                     System.out.println("Not a valid choice. Please enter the number of the above listed options");
             }
 
-            System.out.println("Please enter the number of the operation you'd like to perform:" +
+            System.out.println("\nPlease enter the number of the operation you'd like to perform:" +
                     "\n0: Exit" +
                     "\n1: View Recipe" +
                     "\n2: Add Recipe" +
@@ -83,31 +102,62 @@ public class Main {
         System.exit(0);
     }
 
-    public static void viewRecipe()
+    private static boolean findRecipe(String itemName)
     {
-        System.out.println("Please enter the name of an item to look up the recipe for (Use no spaces, capitalise the beginning of each word, and use singular form)" +
-                "\nEnter 'all' to view all recipes");
+        for(Recipe i : recipes)
+        {
+            if(i.getItemName().equals(itemName))
+                return true;
+        }
+        return false;
+    }
 
+    private static void addRecipe(String itemName)
+    {
         Scanner sc = new Scanner(System.in);
-        String itemName = sc.next();
+        System.out.println("Please enter the output rate of " + itemName + ":");
+        float outputRate = sc.nextFloat();
+        System.out.println("Please enter the building it is produced in: ");
+        String building = sc.next();
+        System.out.println("Please enter the name of input item 1:");
+        String item1Name = sc.next();
+        System.out.println("Please enter the rate of input item 1:");
+        float item1Rate = sc.nextFloat();
+        System.out.println("If there are more input items, enter the name of the next one, otherwise enter 'exit':");
+        String item2Name = sc.next();
+        if(item2Name.equals("exit"))
+        {
+            recipes.add(new Recipe(itemName,outputRate,building,item1Name,item1Rate));
+        }
+        else
+        {
+            System.out.println("Enter the rate of input item 2:");
+            float item2Rate = sc.nextFloat();
+            recipes.add(new Recipe(itemName,outputRate,building,item1Name,item1Rate,item2Name,item2Rate));
+        }
+        System.out.println("Recipe added!");
+
+    }
+
+    private static void viewRecipe(String itemName)
+    {
         if(itemName.equals("all"))
         {
             for(Recipe i : recipes)
             {
                 System.out.println(i);
             }
+            return;
         }
         else {
-            boolean found = false;
             for (Recipe i : recipes) {
                 if (i.getItemName().equals(itemName)) {
                     System.out.println("Recipe found!\n" + i);
-                    found = true;
+                    return;
                 }
             }
-            if (found == false) {
-                System.out.println("Sorry, could not find the recipe for that item :(");
-            }
+            System.out.println("Sorry, could not find the recipe for that item :(");
+            return;
         }
     }
 }
